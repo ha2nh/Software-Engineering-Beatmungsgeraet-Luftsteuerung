d1efcd9af1dafc765389527c96000c27f9ea1b80
ha2nh <mai.tran2@stud.th-deg.de>
2025-11-06 18:45:15 +0100

    Update Modulstruktur.cpp

--- src/Modulstruktur.cpp
+++ src/Modulstruktur.cpp
@@ -1,1 +1,16 @@
-
+│   ├── main.cpp
+│   ├── Luftsteuerung/
+│   │   ├── Luftsteuerung.h
+│   │   └── Luftsteuerung.cpp
+│   ├── Luftmengeverwaltung/
+│   │   ├── Luftmengeverwaltung.h
+│   │   └── Luftmengeverwaltung.cpp
+│   ├── Luftdruckueberwachung/
+│   │   ├── Luftdruckueberwachung.h
+│   │   └── Luftdruckueberwachung.cpp
+│   ├── Dauerverwaltung/
+│   │   ├── Dauerverwaltung.h
+│   │   └── Dauerverwaltung.cpp
+│   └── Alarm/
+│       ├── Alarm.h
+│       └── Alarm.cpp
